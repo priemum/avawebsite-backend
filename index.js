61f424acaa248c5b4f7191aaf58b24cb4b9ee083
Kanaan Alahmad <kanaan92@hotmail.com>
2024-01-06 15:03:31 +0400

    K-Fixes-PaymentPlan-v2.0

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ app.use(credentials);
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.set("trust proxy", true);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: false }));
 //serve static files
 // console.log(path.join(__dirname, "/public"));
 app.use("/public", express.static(path.join(__dirname, "/public")));
