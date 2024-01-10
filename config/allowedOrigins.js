deaa39631d232631058ee1d5dd79700dc762365a
Kanaan Alahmad <kanaan92@hotmail.com>
2024-01-10 18:04:28 +0400

    k-alloweorigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,9 +4,11 @@ const allowedOrigins = [
 	"http://localhost:5173",
 	"http://localhost:5174",
 	"https://www.avarealestate.ae",
+	"https://www.avaproperties.ae",
 	"https://dashboard.avarealestate.ae",
 	"https://ava-frontend-five.vercel.app",
 	"https://ava-dashboard-ten.vercel.app",
+	"https://dashboard.avarealestate.ae",
 	"https://avawebsite-backend-git-main-kanaan92.vercel.app",
 ];
 
